868ce1613719f0460cf21e2ac93c30066b71994d
imwatsi <imwatsi@gmail.com>
2021-10-06 22:40:55 +0200

    fix api bug

--- hive_attention_tokens/network/api/tokens_api.py
+++ hive_attention_tokens/network/api/tokens_api.py
@@ -1,6 +1,5 @@
 from random import randint
 from jsonrpcserver import method, Result, Success
-from hive_attention_tokens.network.peers import Peers
 
 @method(name="tokens_api.get_content_votes")
 async def get_content_votes(author: str, permlink: str) -> Result:
--- hive_attention_tokens/network/serve.py
+++ hive_attention_tokens/network/serve.py
@@ -5,12 +5,11 @@ from aiohttp import web
 from jsonrpcserver import method, async_dispatch
 from jsonrpcserver.methods import Methods
 
-from hive_attention_tokens.network.api import net
+from hive_attention_tokens.network.api import net_api, tokens_api
 
 
 def run_server(config):
     app = web.Application()
-    #app['db'] = AttentionTokensDb(config)
 
     async def status_report(request):
         report = {
@@ -18,40 +17,14 @@ def run_server(config):
             'timestamp': datetime.utcnow().isoformat()
         }
         return web.json_response(status=200, data=report)
+    
+    
+    async def handle(request):
+        return web.Response(
+            text=await async_dispatch(await request.text()), content_type="application/json"
+        )
 
-    async def handler(request):
-        try:
-            #response = await async_dispatch(request, methods=all_methods, debug=True, context=app)
-            return web.Response(
-                text=await async_dispatch(await request.text()),
-                content_type="application/json",
-                headers = {'Access-Control-Allow-Origin': '*'}
-            )
-        except Exception as e:
-            print(e)
-
-        """
-            # create and send error response
-            error_response = {
-                "jsonrpc":"2.0",
-                "error" : {
-                    "code": -32602,
-                    "data": "Invalid JSON in request: " + str(ex),
-                    "message": "Invalid parameters"
-                },
-                "id" : -1
-            }
-            headers = {
-                'Access-Control-Allow-Origin': '*'
-            }
-            ret = web.json_response(error_response, status=200, headers=headers, dumps=decimal_serialize)
-            if req_res_log is not None:
-              req_res_log.info("Request: {} processed in {:.4f}s".format(request, perf_counter() - t_start))
-            return ret
-        """
-
-
-    app.router.add_post("/", handler)
+    app.router.add_post("/", handle)
     app.router.add_get("/", status_report)
     if config['ssl_cert'] != '' and config['ssl_key'] != '':
         context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS)
@@ -64,6 +37,6 @@ def run_server(config):
     web.run_app(
         app,
         host=config['server_host'],
-        port=3636,
+        port=config['server_port'],
         ssl_context=context
     )
