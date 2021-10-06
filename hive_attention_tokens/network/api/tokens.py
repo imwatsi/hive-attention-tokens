5184a9c7764f11e5ec7ccde40cbe13a9f8aab30f
imwatsi <imwatsi@gmail.com>
2021-10-06 21:11:23 +0200

    fix endpoint

--- hive_attention_tokens/network/api/tokens.py
+++ hive_attention_tokens/network/api/tokens.py
@@ -3,6 +3,6 @@ from jsonrpcserver import method, Result, Success
 from hive_attention_tokens.network.peers import Peers
 
 @method(name="tokens_api.get_content_votes")
-async def get_content_votes() -> Result:
+async def get_content_votes(author, permlink) -> Result:
     """Returns the list of current active peers of the node."""
-    return Success(randint(1,100))
+    return Success([{"SK3636363636": randint(1,100)}])
